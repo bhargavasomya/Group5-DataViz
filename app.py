bdf9baa5098cd868a0f32d37b6c8c827c59f4acc
Bagus Trihatmaja <astungkara.project@gmail.com>
2019-03-20 15:23:56 -0400

    First page (Form)

--- app.py
+++ app.py
@@ -1,8 +1,9 @@
-from flask import Flask
+from flask import Flask, request
 from flask_restful import Resource, Api
 from flask_cors import CORS
 
-from sentences import Sentences
+from ML.sentences import Sentences
+from ML.lsa import LSA
 
 app = Flask(__name__)
 api = Api(app)
@@ -11,18 +12,33 @@ CORS(app)
 data = Sentences()
 
 
-class ScatterPlot(Resource):
+class ChartsAPI(Resource):
     def get(self):
+        pass
+
+    @app.route('/charts/scatterplot/', method=['GET'])
+    def scatter_plot(self):
         return data.get_all_sentences()
 
 
-class ClosestSentences(Resource):
-    def get(self, k):
-        return data.get_closest_sentences(k)
+class ClosestSentencesAPI(Resource):
+    def get(self):
+        pass
+
+    @app.route('/closestsentences/<int:k>', method=['GET'])
+    def closest(self, k):
+        content = request.get_json()
+        q1 = LSA.transform(content.get("q1"))
+        q2 = LSA.transform(content.get("q2"))
+
+        q1_closest = data.get_closest_sentences(q1, k=k)
+        q2_closest = data.get_closest_sentences(q2, k=k)
+
+        return q1_closest, q2_closest
 
 
-api.add_resource(ScatterPlot, '/scatterplot/')
-api.add_resource(ScatterPlot, '/closestsentences/<int:k>')
+api.add_resource(ChartsAPI, '/charts/scatterplot', endpoint='scatterplot')
+api.add_resource(ClosestSentencesAPI, '/closestsentences/<int:k>', endpoint='closest')
 
 if __name__ == '__main__':
     app.run()
--- frontend/README.md
+++
@@ -1,3 +0,0 @@
-A [d3.js](https://d3js.org) dashboard using React built with [Create React App](https://github.com/facebookincubator/create-react-app).
-
-`npm i` and `npm start` from the `/reactd3` and you should see a dashboard at `localhost:3000`
