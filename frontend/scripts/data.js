a33c26113ce2549941990d31c60f0bcc6d2e89c1
Bagus Trihatmaja <astungkara.project@gmail.com>
2019-04-06 23:48:43 -0400

    Fix visual bug and duplicate number of points

--- frontend/scripts/data.js
+++ frontend/scripts/data.js
@@ -58,7 +58,7 @@ function processScatterPlot(data) {
     dispatch.call('dataLoaded', null, data);
 }
 
-function getData(q1, q2, k) {
+function getData(q1, q2, k, scroll = false) {
   $.ajax({
     type: "POST",
     contentType: "application/json;charset=utf-8",
@@ -74,9 +74,12 @@ function getData(q1, q2, k) {
       processHistogram(json_data);
       processScatterPlot(json_data);
       $("#scatter-viz").show();
-      document.getElementById("scatter-viz").scrollIntoView({behavior: "smooth"});
+      if (scroll) {
+        document.getElementById("scatter-viz").scrollIntoView({behavior: "smooth"});
+      }
       document.getElementById("Submit").value = "Start";
       $("#spinner1").hide();
+      $("#spinner2").hide();
     },
     error: function(error) {
       console.log(error);
@@ -85,16 +88,25 @@ function getData(q1, q2, k) {
 }
 
 
-$('.dropdown-item').click(function() {
+$('.dropdown1').click(function() {
   // Change sentence
   $("#spinner1").show();
-  $('#pointsDropdown').text($(this).text());
+  $('#points-dropdown1').text($(this).text());
   var q1 = $('#q1FormInput').val();
   var q2 = $('#q2FormInput').val();
   var k = $(this).text();
   getData(q1, q2, k);
 });
 
+$('.dropdown2').click(function() {
+  // Change sentence
+  $("#spinner2").show();
+  $('#points-dropdown2').text($(this).text());
+  var q1 = $('#q1FormInput').val();
+  var q2 = $('#q2FormInput').val();
+  var k = $(this).text();
+  getData(q1, q2, k);
+});
 
 $('#Submit').click(function() {
     document.getElementById("Submit").value = "Loading ...";
@@ -102,7 +114,7 @@ $('#Submit').click(function() {
     var q1 = $('#q1FormInput').val();
     var q2 = $('#q2FormInput').val();
 
-    getData(q1, q2, 1000);
+    getData(q1, q2, 1000, true);
 
 });
 
